7f6e5092ff1e701bea65fc8316ec227452eaa295
SammLopes <sam.v.p.lopes@gmail.com>
2025-05-27 23:05:53 -0300

    Adicionado basename para App.tsx, e configurações de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/file-upload-fiesta">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
